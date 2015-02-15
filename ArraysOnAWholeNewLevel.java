5a2445d990d3eca6f08e69aaeced544f7e4f445a
Kevin Tran <KTOmega@users.noreply.github.com>
2015-02-14 17:20:45 -0800

    Rectangular arrays is an interesting way to put it.

--- ArraysOnAWholeNewLevel.java
+++ ArraysOnAWholeNewLevel.java
@@ -5,19 +5,20 @@ package progfest;
  */
 public class ArraysOnAWholeNewLevel {
     public static void main(String[] args) {
-        // Creates a new array that holds 10 items, from index 0 to 9
+        // Creates a new one-dimensional array that holds 10 items, from index 0 to 9
         int[] oneD = new int[10];
-        // Creates a rectangular array with 10 rows and 5 colums
+        // Creates a two-dimensional array with 10 rows and 5 columns
+        // Think of it as a table.
         int[][] twoD = new int[10][5];
         
-        //Fills the 1D array from 0-9
+        // Fills the 1D array from 0-9
         for (int i = 0; i < oneD.length; i++) {
             oneD[i] = i;
         }
         
         // Fills the 2D array with the product of the row and column index
         for (int row = 0; row < twoD.length; row++) {
-            // twoD[0].length gets the length of the 0th col, which is the
+            // twoD[0].length gets the length of the 1st col, which is the
             // same size as any other column as this is a rectangular array
             for (int col = 0; col < twoD[0].length; col++) {
                 twoD[row][col] = row * col;
